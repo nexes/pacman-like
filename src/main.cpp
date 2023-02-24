1210442e7f4f8d1ce1748f20e42c69d79e28e3a2
Joe Berria <joeberria@gmail.com>
2023-02-24 11:57:15 -0800

    correct client side startup

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@ int main(int argc, const char* argv[])
 {
     Pacman pacman;
 
-    pacman.setupNetwork();
-    pacman.setupUI(150, 80);
+    // we need to establish a connection before anything else
+    // if this fails an error message will be printed and we'll exit
+    if (!pacman.setupNetwork())
+        return 1;
 
+    pacman.getUserName();
     pacman.run();
 
     return 0;
