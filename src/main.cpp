cd4b98266b8aa12562cd7e5b814ce5c513731451
Joe Berria <joeberria@gmail.com>
2023-02-27 19:57:14 -0800

    get player name before network setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@ int main(int argc, const char* argv[])
 {
     Pacman pacman;
 
+    // get the users player name
+    pacman.getUserName();
+
     // we need to establish a connection before anything else
     // if this fails an error message will be printed and we'll exit
     if (!pacman.setupNetwork())
         return 1;
 
-    pacman.getUserName();
     pacman.run();
 
     return 0;
