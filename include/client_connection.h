f3229c79cb8f3cc0d00189f03936906878141c5a
Joe Berria <joeberria@gmail.com>
2023-02-27 19:58:25 -0800

    lock the critical sections between threads

--- include/client_connection.h
+++ include/client_connection.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mutex>
 #include <string>
 #include <vector>
 
@@ -15,7 +16,7 @@ public:
 
     // send a request to the pacman server requesting a new player be setup.
     // this will return a unique player ID and the game map
-    bool requestNewPlayer();
+    bool requestNewPlayer(std::string);
 
     // todo
     void requestDisconnectPlayer();
@@ -27,15 +28,33 @@ public:
     std::string getErrorMsg();
 
     // return the map retrieved from the server
-    std::vector<std::string> getGameMap() const;
+    std::vector<std::string> getGameMap();
+
+    bool hasOpponent();
 
 private:
-    // socket to the server
-    int socket_fd;
-    // unique client id from the server
-    int client_id;
-    // client map
-    std::vector<std::string> client_map;
+    struct CriticalSection
+    {
+        // mutex to lock when writing data
+        std::mutex thread_mutex;
+        // socket to the server we listen on a second thread
+        int socket_fd;
+        // if this player has an opponent
+        bool opponent;
+        // unique client id from the server
+        int client_id;
+        // client map
+        std::vector<std::string> client_map;
+    };
+
+    // listen on a second thread for server responses
+    void thread_listenToServer();
+
+private:
+    // listening to the server
+    bool listening;
     // error message if there are any
     std::string err_msg;
+    // shared data between threads
+    CriticalSection thread_data;
 };
--- src/client_connection.cpp
+++ src/client_connection.cpp
@@ -7,17 +7,17 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <thread>
 
 #include "../include/serializer.h"
 
-ClientConnection::ClientConnection()
-    : socket_fd(-1), err_msg(""), client_id(-1), client_map()
+ClientConnection::ClientConnection() : listening(false), err_msg("")
 {
 }
 
 ClientConnection::~ClientConnection()
 {
-    close(socket_fd);
+    close(this->thread_data.socket_fd);
 }
 
 bool ClientConnection::setupConnection()
@@ -41,72 +41,117 @@ bool ClientConnection::setupConnection()
     }
 
     // setup the client socket fd
-    socket_fd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
-    if (socket_fd == -1) {
-        err_msg = std::string("socket: ").append(gai_strerror(socket_fd));
+    int sock = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+    if (sock == -1) {
+        err_msg = std::string("socket: ").append(gai_strerror(sock));
         freeaddrinfo(info);
 
         return false;
     }
+
+    // there are no threads at this point in the setup, so not locking this
+    this->thread_data.socket_fd = sock;
 
     // set a timeout for reading on the socket for 50ms
     struct timeval tv;
     tv.tv_sec = 0;
     tv.tv_usec = 50000;  // 50 ms
-    status =
-        setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const void *)&tv, sizeof(tv));
+    status = setsockopt(this->thread_data.socket_fd,
+                        SOL_SOCKET,
+                        SO_RCVTIMEO,
+                        (const void *)&tv,
+                        sizeof(tv));
 
     // connect to the server
-    status = connect(socket_fd, info->ai_addr, info->ai_addrlen);
+    status = connect(this->thread_data.socket_fd, info->ai_addr, info->ai_addrlen);
     if (status == -1) {
         err_msg = std::string("connect: ").append(strerror(status));
         freeaddrinfo(info);
-        close(socket_fd);
+        close(this->thread_data.socket_fd);
         return false;
     }
 
     freeaddrinfo(info);
+
+    // start a new thread to listen for server responses
+    this->listening = true;
+    std::thread t(&ClientConnection::thread_listenToServer, this);
+    t.detach();  // TODO: proper thread handling
+
     return true;
 }
 
 // send a request to the server for a new player id and get the map data.
 // if this send/recv failed map data can't load, needs to be resent
-bool ClientConnection::requestNewPlayer()
+bool ClientConnection::requestNewPlayer(std::string playername)
 {
+    int wrote = 0;
     int len = 0;
     int sent = 0;
-    bool success = true;
 
     // serialize the send request data;
-    SerializedData player_data = Serializer::SerializeNewPlayer();
+    SerializedData player_data = Serializer::SerializeNewPlayerRequest(playername);
 
     // send the newplayer request to the server
     do {
-        int s = write(this->socket_fd, (void *)player_data.data, player_data.len);
-        if (s == -1) {
-            err_msg = std::string("NewPlayer request: ").append(strerror(s));
-            success = false;
-            break;
+        {
+            std::lock_guard<std::mutex> lock(this->thread_data.thread_mutex);
+            wrote = write(this->thread_data.socket_fd,
+                          (void *)player_data.data,
+                          player_data.len);
+        }  // unlock critical section here
+
+        if (wrote == -1) {
+            err_msg = std::string("NewPlayer request: ").append(strerror(wrote));
+            return false;
         }
-        sent += s;
+        sent += wrote;
     } while (sent != player_data.len);
 
-    // get the response
-    char response[ServerInfo::player_read_size];
-    int r = read(this->socket_fd, (void *)response, sizeof(response));
-    if (r == -1) {
-        err_msg = std::string("NewPlayer request: ").append(strerror(r));
-        success = false;
+    return true;
+}
+
+void ClientConnection::thread_listenToServer()
+{
+    while (listening) {
+        char response[ServerInfo::player_read_size];
+
+        // get the response if there is any
+        int r = read(this->thread_data.socket_fd, (void *)response, sizeof(response));
+
+        // no response yet
+        if (r < 1) {
+            std::this_thread::sleep_for(std::chrono::microseconds(GameInfo::ThreadSleep));
+            continue;
+        }
+
+        int type = *(int *)response;
+
+        switch (type) {
+        case RequestType::NewPlayer: {
+            NewPlayerData newPlayer = Serializer::DeSerializeNewPlayerResponse(response);
+
+            std::lock_guard<std::mutex> lock(this->thread_data.thread_mutex);
+            this->thread_data.client_id = newPlayer.userID;
+            this->thread_data.client_map = newPlayer.map;
+            this->thread_data.opponent = newPlayer.has_opponent;
+
+            break;
+        }
+        case RequestType::NewOpponent: {
+            std::lock_guard<std::mutex> lock(this->thread_data.thread_mutex);
+            this->thread_data.opponent = true;
+            break;
+        }
+        case RequestType::UpdatePlayer:
+            break;
+        case RequestType::DisconnectPlayer:
+            break;
+        }
+
+        // sleep
+        std::this_thread::sleep_for(std::chrono::microseconds(GameInfo::ThreadSleep));
     }
-
-    // deserialize the byte stream to a usable data struct
-    DeSerializedData data = Serializer::DeSerializeNewPlayerResponse(response);
-    this->client_id = data.userID;
-    this->client_map = data.map;
-
-    // TODO: handle bad response data
-
-    return success;
 }
 
 std::string ClientConnection::getErrorMsg()
@@ -114,7 +159,24 @@ std::string ClientConnection::getErrorMsg()
     return this->err_msg;
 }
 
-std::vector<std::string> ClientConnection::getGameMap() const
+std::vector<std::string> ClientConnection::getGameMap()
 {
-    return this->client_map;
+    std::vector<std::string> map;
+    {
+        std::lock_guard<std::mutex> lock(this->thread_data.thread_mutex);
+        map = this->thread_data.client_map;
+    }
+
+    return map;
+}
+
+bool ClientConnection::hasOpponent()
+{
+    bool op = false;
+    {
+        std::lock_guard<std::mutex> lock(this->thread_data.thread_mutex);
+        op = this->thread_data.opponent;
+    }
+
+    return op;
 }
